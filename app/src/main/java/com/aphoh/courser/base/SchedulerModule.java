c1dd917b99291eacfc8ce45c60b82b5e1425f63e
William Arnold <Will748@gmail.com>
2015-09-11 13:08:25 -0700

    Forgot to commit SchedulerModule

---
+++ app/src/main/java/com/aphoh/courser/base/SchedulerModule.java
@@ -0,0 +1,18 @@
+package com.aphoh.courser.base;
+
+import dagger.Module;
+import dagger.Provides;
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+
+/**
+ * Created by Will on 9/10/15.
+ */
+@Module
+public class SchedulerModule {
+
+    @Provides
+    public Scheduler provideSubscriptionScheduler(){
+        return AndroidSchedulers.mainThread();
+    }
+}
