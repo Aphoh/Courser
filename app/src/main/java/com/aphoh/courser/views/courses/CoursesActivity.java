90f3b1b02fd5e2197d3cb3252af9e3946e64ac93
William Arnold <Will748@gmail.com>
2015-09-16 10:58:48 -0700

    Add request back into CoursesActivity

--- app/src/main/java/com/aphoh/courser/views/courses/CoursesActivity.java
+++ app/src/main/java/com/aphoh/courser/views/courses/CoursesActivity.java
@@ -69,6 +69,8 @@ public class CoursesActivity extends NucleusActivity<CoursesPresenter> implement
                         .show();
             }
         });
+
+        getPresenter().requestCourses();
     }
 
     @Override
